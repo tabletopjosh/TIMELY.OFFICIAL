8fb8f8f385f8debbd29eb6bde3bb356909f6b969
tabletopjosh <joshuaarounI@gmail.com>
2024-05-20 17:54:40 -0400

    Initial Commit

---
+++ TIMELY.OFFICIAL/TIMELY_OFFICIALApp.swift
@@ -0,0 +1,17 @@
+//
+//  TIMELY_OFFICIALApp.swift
+//  TIMELY.OFFICIAL
+//
+//  Created by Sa38 on 5/20/24.
+//
+
+import SwiftUI
+
+@main
+struct TIMELY_OFFICIALApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
