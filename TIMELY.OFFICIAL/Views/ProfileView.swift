f56b851647da1600360579c490b1028f665e78c8
tabletopjosh <joshuaarounI@gmail.com>
2024-05-22 17:29:51 -0400

    Website Foundation: Tab Bar

---
+++ TIMELY.OFFICIAL/Views/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  TIMELY.OFFICIAL
+//
+//  Created by Sa38 on 5/22/24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
